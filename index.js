e5bb90f8a1b2e1829bb17c57490e6d10ff42b2a0
Laura Trivino <laura.trivino@wanadoo.fr>
2023-01-15 17:00:38 +0100

    [PLAYGROUND DEMO] Bases de commandes et intéractions pour apprendre à manipuler le bot

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 const { TOKEN_LOGIN } = require("./apikey.js");
+const {SlashCommandBuilder } = require("@discordjs/builders");
 
 const client = new Client({
     intents: [
@@ -9,25 +10,69 @@ const client = new Client({
     ]
 });
 
+//démarrage du bot
+/*
+Point sur les commandes au lancement du bot :
+   //client.guilds.cache.get("idserver").commands.create(data); //via l'id du serveur ça peut prendre moinds de temps
+
+    //lient.guilds.cache.get("idserver").commands.fetch(); // recharger les commandes dans le cache
+
+    //client.guilds.cache.get("idserver").commands.cache; // pour obtenir le cache
+    
+    //client.guilds.cache.get("idserver").commands.cache.map(command => { command.delete();}) //supprimer les commandes dans le cache 
+*/
 client.on("ready", () => {
+
+    client.application.commands.create(data); 
+
+ 
     console.log("bot opérationnel");
 })
+//
 
+//Commande
+const data = new SlashCommandBuilder()
+    .setName("camion")
+    .setDescription("blague très mature")
+    .addUserOption(option => option
+        .setName("utilisateur")
+        .setDescription("utilisateur à pouet pouet")
+        .setRequired(false))
+        ;
+
+
+client.on("interactionCreate", interaction => {
+    if(interaction.isCommand()){
+       if(interaction.commandName === "camion"){
+            let paramUser = interaction.options.getUser("utilisateur");
+            if( paramUser != undefined){
+                
+               interaction.reply("<@"+ paramUser.id +"> pouet pouet");
+
+            }else{
+
+                interaction.reply("pouet pouet");
+            }
+
+         }
+    }
+});
+
+//    
+
+//simples intércations avec la lecture d'un message
 client.on("messageCreate", message => {
+
     if(message.author.bot) return;
-    console.log(message);
+
     if(message.content==="Marco"){
-        message.reply("Polo");
-        message.channel.send("Polo");
-        console.log("Polo");
+
+        message.reply("Polo"); //methode 1
     }
     if(message.content==="ping"){
         
-        message.reply("pong");
-        console.log("pong");
-        message.channel.send("pong");
+        message.channel.send("pong"); //methode 2
     }
-
 
     if(message.content === "help"){
         message.reply("Liste des commandes bots : \n");
@@ -37,6 +82,10 @@ client.on("messageCreate", message => {
     if( message.content === "mention"){
         message.reply("Mention du user : <@"+message.author.id + "> \n Mention d'un salon : <#" + message.channel.id + ">" );
     }
+
+    if(message.content === "pan"){
+      message.reply("ET PAN UNE CLAQUETTE DANS LA TÊTE DE <@147059697776590848>"); //pauvre Draeky
+    }
 })
 
-client.login(TOKEN_LOGIN) 
+client.login(TOKEN_LOGIN) //connexion du bot à l'api discord
