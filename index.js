31e48a9d76a38c70b366366a0da1aaafaaa549ac
Laura Trivino <laura.trivino@wanadoo.fr>
2023-01-15 15:55:04 +0100

    [SECRETS] move apikey to local file gitignored

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const { Client, GatewayIntentBits } = require("discord.js");
+const { TOKEN_LOGIN } = require("./apikey.js");
+
 const client = new Client({
     intents: [
-        GatewayIntentBits.Guilds
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
     ]
 });
 
@@ -9,4 +13,30 @@ client.on("ready", () => {
     console.log("bot opérationnel");
 })
 
-client.login("") //mettre la clé du discord en secret
+client.on("messageCreate", message => {
+    if(message.author.bot) return;
+    console.log(message);
+    if(message.content==="Marco"){
+        message.reply("Polo");
+        message.channel.send("Polo");
+        console.log("Polo");
+    }
+    if(message.content==="ping"){
+        
+        message.reply("pong");
+        console.log("pong");
+        message.channel.send("pong");
+    }
+
+
+    if(message.content === "help"){
+        message.reply("Liste des commandes bots : \n");
+        //TODO boucle sur les différentes commandes, à orga quelque part pour que ça soit pas à la main, tableau de correspondance des commandes à une fonction ? 
+    }
+
+    if( message.content === "mention"){
+        message.reply("Mention du user : <@"+message.author.id + "> \n Mention d'un salon : <#" + message.channel.id + ">" );
+    }
+})
+
+client.login(TOKEN_LOGIN) 
