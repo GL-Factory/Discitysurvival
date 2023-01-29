b5a3aef58d9fcc2b53fd65f97917914e3127851f
Gautier <gautier.dran@gmail.com>
2023-01-29 12:40:32 +0100

    add first version of the explore function

---
+++ commands/explore.js
@@ -0,0 +1,40 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const wait = require('node:timers/promises').setTimeout;
+
+module.exports = {
+	data: new SlashCommandBuilder()
+    .setName("explore")
+    .setDescription("Permet à l'utilisateur d'explorer le monde"),
+	async execute(interaction) {
+        if(interaction.isCommand()){
+            if(interaction.commandName === "explore"){ 
+                let user = interaction.user;
+                //let user = interaction.targetUser;
+                console.log("Utilisateur : " + user.username + " pars en exploration");
+                let random = Math.floor(Math.random() * 101);
+                console.log("Nombre aléatoire généré pour l'utilisateur " + user.username + " est :" + random);
+                if (random <= 10) {
+                    await interaction.reply({ content: 'Vous ne trouvez rien', ephemeral: true });
+                } else if (random > 10 && random <= 30) {
+                    await interaction.reply({ content: 'Vous trouvez des ressources', ephemeral: true });
+                } else if (random > 30 && random <= 50) {
+                    await interaction.reply({ content: 'Vous trouvez des rations', ephemeral: true });
+                } else if (random > 50 && random <=75) {
+                    await interaction.reply({ content: 'Un monstre vous attaque', ephemeral: true });
+                } else if (random > 75 && random <=100) {
+                    await interaction.reply({ content: 'Vous êtes tombé dans un piège', ephemeral: true });
+                }
+                // 10% chance rien trouver
+                // 20% chance trouver resources
+                // 20% chance trouver ration
+                // 25% chance Monstre et bagarre
+                // 25% chance de tomber dans un piège
+
+
+            } else {
+                interaction.reply({ content: 'Une erreur etrange c\'est glissée ici', ephemeral: true});
+            }
+        }  
+    }
+};
