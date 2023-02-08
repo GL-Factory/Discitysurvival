b30eea87ac01187bfba34f37734005506dcf4b27
John KLV <john.klv.org@gmail.com>
2023-02-08 15:26:08 +0100

    feat: add functions in city to manage door

--- models/city.js
+++ models/city.js
@@ -5,7 +5,8 @@ let state = {
         "food" : 0,
         "kit" : 0,
         "wood" : 0
-    }
+    },
+    "doorOpen": false
 }
 function enrollNewPlayer(username){
     if(isEnrolled(username)) {
@@ -26,11 +27,21 @@ function enrollNewPlayer(username){
             "thirst": 0 // 0 Hydraté / 1 Acceptable / 2 Déshydraté / 3 Mort
         }, 
         "location": "city" // city / wild for now
-    }
+    } ;
 }
 function isEnrolled(username){
-    return Object.keys(global_storage['player_list']).includes(username)
+    return Object.keys(global_storage['player_list']).includes(username) ;
 }
 
-module.exports = { state, enrollNewPlayer, isEnrolled }
+function isDoorOpen()   { return state.doorOpen ; }
+function doorClose()    { state.doorOpen = false ; }
+function doorOpen()     { state.doorOpen = true ; }
+
+let door = {
+    "isOpen"    : isDoorOpen,
+    "open"      : doorClose,
+    "close"     : doorOpen
+}
+
+module.exports = { state, enrollNewPlayer, isEnrolled, door }
 
